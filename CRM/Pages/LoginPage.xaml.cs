ddda67a406de5767be81108ec030529b55fd8500
Yasin Akmaz <akmazkedy@gmail.com>
2024-12-29 19:03:52 +0300

    29.12.2024 19:03

--- CRM/Pages/LoginPage.xaml.cs
+++ CRM/Pages/LoginPage.xaml.cs
@@ -7,8 +7,10 @@ public partial class LoginPage : ContentPage
 	public LoginPage()
 	{
 		InitializeComponent();
+        GrdRegister.IsEnabled = false;
+        GrdRegister.IsVisible = false;
     }
-    private async void Login()
+    private async void LoginCommand()
 	{
         if (string.IsNullOrWhiteSpace(TxtEmail.Text) || string.IsNullOrWhiteSpace(TxtPassword.Text))
         {
@@ -23,6 +25,10 @@ public partial class LoginPage : ContentPage
             {
                 await Shell.Current.GoToAsync($"{nameof(MenuPage)}");
             }
+            else
+            {
+                await DisplayAlert("Sistem", response ?? "Bilinmeyen bir hata oluþtu.", "Tamam");
+            }
         }
         catch (Exception ex)
         {
@@ -32,6 +38,55 @@ public partial class LoginPage : ContentPage
 
     private void BtnLogin_Clicked(object sender, EventArgs e)
     {
-        Login();
+        LoginCommand();
+    }
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+        if (GrdRegister.IsVisible == false)
+        {
+            GrdGirisYap.IsEnabled = false;
+            GrdGirisYap.IsVisible = false;
+            GrdRegister.IsVisible = true;
+            GrdRegister.IsEnabled = true;
+        }
+        else 
+        {
+            GrdGirisYap.IsEnabled = true;
+            GrdGirisYap.IsVisible = true;
+            GrdRegister.IsVisible = false;
+            GrdRegister.IsEnabled = false;
+        }
+    }
+
+    private async void RegisterCommand()
+    {
+        if (string.IsNullOrWhiteSpace(TxtDisplayName.Text) || string.IsNullOrWhiteSpace(TxtRegEmail.Text) || string.IsNullOrWhiteSpace(TxtRegPassword.Text))
+        {
+            await DisplayAlert("Sistem", "Lütfen gerekli alanlarý doldurunuz!", "Tamam");
+            return;
+        }
+        try
+        {
+            FirebaseAuth auth = new FirebaseAuth();
+            var response = await auth.CreateRegister(TxtDisplayName.Text, TxtRegEmail.Text, TxtRegPassword.Text);
+            if (!string.IsNullOrWhiteSpace(response) && response.StartsWith("Baþarýlý"))
+            {
+               
+            }
+            else
+            {
+                await DisplayAlert("Sistem", response ?? "Bilinmeyen bir hata oluþtu.", "Tamam");
+            }
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Hata", $"Bir hata oluþtu: {ex.Message}", "Tamam");
+        }
+    }
+
+    private void BtnRegister_Clicked(object sender, EventArgs e)
+    {
+        RegisterCommand();
     }
 }
